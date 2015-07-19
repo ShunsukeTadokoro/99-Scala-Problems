601b73d5fd05d143f622aac6a297abb9b73316e3
田所駿佑 <Shunsuke@tadokoroshunyuu-no-iMac.local>
2015-07-20 02:33:32 +0900

    ~ 12

--- src/main/scala/methods.scala
+++ src/main/scala/methods.scala
@@ -5,26 +5,78 @@ import java.util.NoSuchElementException
 object methods {
 
   def last[A](list: List[A]): A = list match {
-    case value :: Nil  => value
+    case value :: Nil => value
     case _ :: tail => last(tail)
-    case _         => throw new NoSuchElementException
+    case _ => throw new NoSuchElementException
   }
 
-  def penultimate[A](list:List[A]) : A = list match {
+  def penultimate[A](list: List[A]): A = list match {
     case value :: _ :: Nil => value
     case _ :: tail => penultimate(tail)
     case _ => throw new NoSuchElementException
   }
 
-  def nth[A](n:Int, list:List[A]): A = (n, list) match {
+  def nth[A](n: Int, list: List[A]): A = (n, list) match {
     case (0, value :: _) => value
-    case (n, _ :: tail) => nth(n-1, tail)
+    case (n, _ :: tail) => nth(n - 1, tail)
     case _ => throw new NoSuchElementException
   }
 
-  def length[A](list:List[A], n:Int = 0): Int = list match {
-    case _ :: Nil => n
-    case _ :: tail => length(tail, n+1)
+  def length[A](list: List[A]): Int = list match {
+    case _ :: Nil => 0
+    case _ :: tail => 1 + length(tail)
   }
+
+  def reverse[A](list:List[A]): List[A] = {
+    list.foldLeft(List.empty[A]) {(a, b) => b :: a}
+  }
+
+  def isPalindrome[A](list:List[A]): Boolean = {
+    list.foldLeft(List.empty[A]) {(a, b) => b :: a} == list
+  }
+
+  def flatten(list: List[_]): List[_] = list match{
+    case List() => List()
+    case (al:List[_]) :: (bl:List[_]) => flatten(al) ::: flatten(bl)
+    case a :: (bl:List[_]) => List(a) ::: flatten(bl)
+  }
+
+  def compress[A](list:List[A]): List[A] = list match {
+    case Nil => Nil
+    case e :: tail => e :: compress(tail.dropWhile(_ == e))
+  }
+
+  def pack[A](list: List[A]): List[List[A]] = {
+    if (list.isEmpty) List(List())
+    else {
+      val (packed, next) = list.span(_ == list.head)
+      if (next == Nil) List(packed)
+      else packed :: pack(next)
+    }
+  }
+
+  def encode[A](list:List[A]): List[(Int, A)] = {
+    pack(list).map(e => (e.length, e.head))
+  }
+
+  def decode[A](list:List[(Int, A)]): List[A] = {
+    list.flatMap(e => Stream.fill(e._1)(e._2))
+  }
+
+  def encodeDirect[A](list:List[A]): List[(Int, A)] = {
+    if (list.isEmpty) Nil
+    else {
+      val (packed, next) = list.span(_ == list.head)
+      (packed.length, packed.head) :: encodeDirect(next)
+    }
+  }
+
+//思い出
+//  def duplicate[A](list: List[A]): List[A] = list match {
+//    case Nil => Nil
+//    case e :: tail => e :: e :: duplicate(tail)
+//  }
+
+  def duplicate[A](list: List[A]): List[A] = list.flatMap(e => List(e, e))
 
 }
--- src/test/scala/testcase.scala
+++ src/test/scala/testcase.scala
@@ -33,5 +33,76 @@ class Testcase extends FunSpec {
     }
   }
 
+  describe("problem5") {
+    it("should return reversed") {
+      val list = List(1,2,3,4,5,6)
+      val expected = List(6,5,4,3,2,1)
+      assert(reverse(list) == expected)
+    }
+  }
+
+  describe("problem6") {
+    it("should return is回文") {
+      val list = List(1,2,3,2,1)
+      assert(isPalindrome(list))
+    }
+  }
+
+  describe("problem7") {
+    it("should return flatten") {
+      val list = List(List(1,2,3), List(4,5))
+      val expected = List(1,2,3,4,5)
+      assert(flatten(list) == expected)
+    }
+  }
+
+  describe("problem8") {
+    it("should return compressed") {
+      val list = List(1,1,2,3,4,5,5)
+      val expected = List(1,2,3,4,5)
+      assert(compress(list) == expected)
+    }
+  }
+
+  describe("problem9") {
+    it("should return packed") {
+      val list = List(1,1,2,3,4,5,5)
+      val expected = List(List(1,1), List(2), List(3), List(4), List(5,5))
+      assert(pack(list) == expected)
+    }
+  }
+
+  describe("problem9") {
+    it("should return encoded") {
+      val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      val expected = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+      assert(encode(list) == expected)
+    }
+  }
+
+  describe("problem10") {
+    it("should return decoded") {
+      val list = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+      val expected = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      assert(decode(list) == expected)
+    }
+  }
+
+  describe("problem11") {
+    it("should return direct encoded") {
+      val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      val expected = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+      assert(encodeDirect(list) == expected)
+    }
+  }
+
+  describe("problem12") {
+    it("should return duplicated") {
+      val list = List('a, 'b, 'b, 'c, 'd, 'd)
+      val expected = List('a, 'a, 'b, 'b, 'b, 'b, 'c, 'c, 'd, 'd, 'd, 'd)
+      assert(duplicate(list) == expected)
+    }
+  }
+
 }
 
